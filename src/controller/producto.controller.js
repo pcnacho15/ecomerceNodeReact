d29e11ca4a7952313d3e7237fb611cdc86a5b1fd
Cristian Camilo Martínez Soto <ccmartinez@SAVIASALUDEPS.LOCAL>
2023-10-15 01:36:26 -0500

    Finalizar peticiones put y delete

--- src/controller/producto.controller.js
+++ src/controller/producto.controller.js
@@ -15,19 +15,38 @@ const verProductos = async (req = request, res = response) => {
     }
 }
 
+const verProducto = async (req = request, res = response) => {
+    const { id } = req.params;
+    try {
+        const productoExiste = await Producto.findByPk(id);
+        if (!productoExiste) {
+            return res.status(400).json({
+                msg: "El producto que intentas ver, no ha sido encontrado."
+            });
+        }
+        res.json({
+            producto: productoExiste
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: "Ha ocurrido un error, hable con un administrador."
+        });
+    }
+}
+
 const agregarProducto = async (req = request, res = response) => {
     const { body } = req;
     body.nombre = body.nombre.toUpperCase();
     try {
         const productoExiste = await Producto.findOne({
-            where : {
-                nombre : body.nombre
+            where: {
+                nombre: body.nombre
             }
         });
-
         if (productoExiste) {
             return res.status(400).json({
-                msg : "El producto que intentas agregar, ya se encuentra registrado.",
+                msg: "El producto que intentas agregar, ya se encuentra registrado.",
                 producto: productoExiste
             });
         }
@@ -44,7 +63,58 @@ const agregarProducto = async (req = request, res = response) => {
     }
 }
 
+const actualizarProducto = async (req = request, res = response) => {
+    const { id } = req.params
+    const { usuario_crea, createdAt, ...resto } = req.body;
+    try {
+        const producto = await Producto.findByPk(id);
+        if (!producto) {
+            return res.status(400).json({
+                msg: "El productos que intentas actualizar, no existe."
+            });
+        }
+
+        resto.nombre = resto.nombre.toUpperCase();
+        await producto.update(resto);
+
+        res.status(200).json({
+            msg: "El producto ha sido actualizado de manera exitosa.",
+            producto
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: "Ha ocurrido un error al intentar actualizar el producto, contacte a un administrador."
+        });
+    }
+}
+
+const eliminarProducto = async (req = request, res = response) => {
+    const { id } = req.params;
+    try {
+        const producto = await Producto.findByPk(id);
+        if (!producto) {
+            res.status(400).json({
+                msg: "El producto no existe."
+            });
+        }
+        await producto.destroy();
+        res.status(200).json({
+            msg: "El producto se ha eliminado de manera exitosa.",
+            producto
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: "Ha ocurrido un error al intentar borrar el producto, contacte con un administrador."
+        });
+    }
+}
+
 module.exports = {
     verProductos,
-    agregarProducto
+    verProducto,
+    agregarProducto,
+    actualizarProducto,
+    eliminarProducto
 }
--- src/routes/producto.routes.js
+++ src/routes/producto.routes.js
@@ -1,12 +1,17 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { verProductos, agregarProducto } = require('../controller/producto.controller');
+const { verProductos, agregarProducto, verProducto, actualizarProducto, eliminarProducto } = require('../controller/producto.controller');
 
 const router = Router();
 
 router.get('/', verProductos);
 
+router.get('/:id', verProducto)
+
 router.post('/', agregarProducto);
 
+router.put('/:id', actualizarProducto);
+
+router.delete('/:id', eliminarProducto);
 
 module.exports = router;
